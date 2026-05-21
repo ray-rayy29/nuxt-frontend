0b9f1dc086635f6a975e7b533479d90d424a1577
ray-rayy29 <rayhannotfadhillah09@gmail.com>
2026-05-21 09:54:15 +0700

    Modify API configuration and add proxy route rules
    Updated API base URL for production and added route rules for proxy.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,25 @@
 import tailwindcss from "@tailwindcss/vite";
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./assets/css/tailwind.css'],
   
-  // Perbaikan di bagian ini 👇
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:8000/api',
+      // Di production, biarkan dia menembak ke url lokal '/api' agar lewat proxy Vercel
+      apiBase: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8000/api',
       storageUrl: process.env.NUXT_PUBLIC_STORAGE_URL || 'http://localhost:8000/storage'
     }
   },
   
-  // Konfigurasi untuk production
+  // TAMBAHKAN ROUTE RULES PROXY INI 👇
+  routeRules: {
+    '/api/**': { 
+      proxy: 'https://laravel-backending.infinityfreeapp.com/api/**' 
+    }
+  },
+  
   nitro: {
     preset: 'vercel',
     compressPublicAssets: true
