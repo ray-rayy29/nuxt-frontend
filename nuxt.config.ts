7d853b4c6e66002cb945396a6b1bbc6d2c8194f6
ray-rayy29 <rayhannotfadhillah09@gmail.com>
2026-05-22 08:47:04 +0700

    Refactor nuxt.config.ts comments and proxy settings
    Updated comments for clarity and adjusted proxy settings.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,19 +7,17 @@ export default defineNuxtConfig({
   
   runtimeConfig: {
     public: {
-      // Di production, biarkan dia menembak ke url lokal '/api' agar lewat proxy Vercel
+      // Saat production di Vercel, arahkan ke rute lokal '/api' agar memicu routeRules
       apiBase: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8000/api',
       storageUrl: process.env.NUXT_PUBLIC_STORAGE_URL || 'http://localhost:8000/storage'
     }
   },
   
-  // PERBAIKAN BLOCK PROXY & HEADERS DI SINI 👇
   routeRules: {
     '/api/**': { 
-      // 1. Alihkan request langsung ke backend InfinityFree
+      // Alihkan request dari Vercel langsung ke backend InfinityFree
       proxy: 'https://laravel-backending.infinityfreeapp.com/api/**',
-      
-      // 2. Suntikkan header browser asli agar dikira manusia oleh proteksi AES Bot InfinityFree
+      // Manipulasi header agar lolos dari verifikasi Bot sistem InfinityFree
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
         'Accept': 'application/json',
